3756cba50a13b3e4c4ea167629964c7b2f045d9c
sunyanguo <dacaiguoguo@163.com>
2023-11-02 21:24:01 +0800

    Initial Commit

---
+++ FindBlock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FindBlock
+//
+//  Created by yanguo sun on 2023/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindBlock/FindBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindBlockApp.swift
+//  FindBlock
+//
+//  Created by yanguo sun on 2023/11/2.
+//
+
+import SwiftUI
+
+@main
+struct FindBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
