466ccda8d6c61bb41b04874cb634eae282db4098
sunyanguo <dacaiguoguo@163.com>
2023-11-02 22:02:52 +0800

    Update FindBlockTests.swift

--- FindBlockTests/FindBlockTests.swift
+++ FindBlockTests/FindBlockTests.swift
@@ -113,6 +113,25 @@ final class FindBlockTests: XCTestCase {
         XCTAssert(ret)
     }
 
+    func testExample6() throws {
+        // 三维数组
+        let array3d = [
+            [[1, 2, 3],    [4, 5, 6],    [7, 8, 9]],
+            [[21, 21, 21], [24, 25, 26], [27, 28, 33]],
+            [[21, 32, 33], [34, 35, 36], [37, 38, 39]],
+        ]
+
+        // 映射成三维 PointInfo 类的实例
+        let pointInfo3DArray = mapTo3DPointInfo(array3d: array3d)
+        // 示例用法
+        let hasContinuousEqual = hasContinuousEqualValues(pointInfo3DArray: pointInfo3DArray)
+        print("是否存在连续三个相等的值: \(hasContinuousEqual)")
+
+        let ret = hasContinuousEqualValues(pointInfo3DArray: pointInfo3DArray)
+        print(ret)
+        XCTAssert(ret)
+    }
+
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
